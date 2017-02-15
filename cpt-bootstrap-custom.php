517dd57318c3369c2fdf56ac55aaa35bb6356311
Adrian <catspajamastech@users.noreply.github.com>
2017-02-15 15:33:57 -0800

    Shortened CSS handle name to 'bootstrap'

--- cpt-bootstrap-custom.php
+++ cpt-bootstrap-custom.php
@@ -9,11 +9,11 @@
  * License: MIT
  */
  
-// register script and style from CDN
+// register scripts and styles from CDN
 wp_register_script('bootstrap-js','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js','jquery','3.3.7','');
-wp_register_style ('bootstrap-css','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css','','3.3.7','');
+wp_register_style ('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css','','3.3.7','');
 
-// enqueue scripts and style
+// enqueue scripts and styles
 wp_enqueue_script('jquery');
 wp_enqueue_script('bootstrap-js');
-wp_enqueue_style('bootstrap-css');
+wp_enqueue_style('bootstrap');
