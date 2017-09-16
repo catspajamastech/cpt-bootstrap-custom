a4ec41b154d42ebc5cb09e900aa7b9b858e3aed0
CPT2 <cptlogin2017@gmail.com>
2017-09-16 03:38:49 +0000

    Uses wp_enqueue_scripts & add_action to hook bootstrap js & css to front end only.

--- cpt-bootstrap-custom.php
+++ cpt-bootstrap-custom.php
@@ -2,18 +2,23 @@
 /**
  * Plugin Name: CPT Bootstrap (Custom)
  * Plugin URI: https://github.com/catspajamastech/cpt-bootstrap-custom/
- * Description: A minimal bootstrap plugin for Wordpress. Forget that silly business of editing functions.php.
- * Version: 3.3.7.02152017
+ * Description: A minimal bootstrap plugin for Wordpress that loads from CDN. Forget that silly business of editing functions.php.
+ * Version: 3.3.7.09152017
  * Author: Cat's Pajamas Technology
  * Author URI: https://catspajamas.tech/
  * License: MIT
  */
  
-// register scripts and styles from CDN
-wp_register_script('bootstrap-js','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js','jquery','3.3.7','');
-wp_register_style ('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css','','3.3.7','');
+if(!defined('ABSPATH')){exit;}//Exit if accessed directly
 
-// enqueue scripts and styles
-wp_enqueue_script('jquery');
-wp_enqueue_script('bootstrap-js');
-wp_enqueue_style('bootstrap');
+function load_bootstrap() {
+    // register scripts and styles from CDN
+    wp_register_script('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js','jquery','3.3.7','');
+    wp_register_style ('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css','','3.3.7','');
+
+    // enqueue scripts and styles
+    //wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap');
+    wp_enqueue_style('bootstrap');
+};
+add_action('wp_enqueue_scripts', 'load_bootstrap');
