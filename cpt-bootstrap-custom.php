c28e1ee7e5a7d6ad0746f14780f5a916aacc67e4
Adrian <catspajamastech@users.noreply.github.com>
2017-09-19 10:35:59 -0700

    Relies on WP jquery dependency

--- cpt-bootstrap-custom.php
+++ cpt-bootstrap-custom.php
@@ -3,21 +3,19 @@
  * Plugin Name: CPT Bootstrap (Custom)
  * Plugin URI: https://github.com/catspajamastech/cpt-bootstrap-custom/
  * Description: A minimal bootstrap plugin for Wordpress that loads from CDN. Forget that silly business of editing functions.php.
- * Version: 3.3.7.09152017
+ * Version: 3.3.7.09192017
  * Author: Cat's Pajamas Technology
  * Author URI: https://catspajamas.tech/
  * License: MIT
  */
  
 if(!defined('ABSPATH')){exit;}//Exit if accessed directly
-
 function load_bootstrap() {
     // register scripts and styles from CDN
     wp_register_script('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js','jquery','3.3.7','');
     wp_register_style ('bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css','','3.3.7','');
-
     // enqueue scripts and styles
-    //wp_enqueue_script('jquery');
+    wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap');
     wp_enqueue_style('bootstrap');
 };
